a7f1bc1a1958babf8387d5a48587d4dae95f6442
Felix Menges <felix-menges@uiowa.edu>
2025-04-05 23:19:39 -0500

    Added Flask

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""This is a simple Flask application that returns 'hello world'"""
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -5,8 +6,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
+@app.route("/submit_name", methods=["POST"])
+def display_name():
+    print("in display_name function")
+    if request.method == "POST":
+        print("in post method")
+        form = request.form
+        print("extracted form data")
+        print(form)
+        user_name = form["my_name"]
+        print(user_name)
+        return "done"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
